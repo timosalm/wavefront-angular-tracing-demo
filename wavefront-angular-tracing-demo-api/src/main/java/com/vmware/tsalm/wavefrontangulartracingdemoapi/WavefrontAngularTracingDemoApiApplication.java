0aac7894f6a3330c5ac4660b48ae0f04b9b250c1
Timo Salm <tsalm@pivotal.io>
2020-07-14 11:25:28 +0200

    Add initial API implementation

---
+++ wavefront-angular-tracing-demo-api/src/main/java/com/vmware/tsalm/wavefrontangulartracingdemoapi/WavefrontAngularTracingDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tsalm.wavefrontangulartracingdemoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavefrontAngularTracingDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavefrontAngularTracingDemoApiApplication.class, args);
+	}
+
+}
---
+++ wavefront-angular-tracing-demo-api/src/test/java/com/vmware/tsalm/wavefrontangulartracingdemoapi/WavefrontAngularTracingDemoApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.vmware.tsalm.wavefrontangulartracingdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = "management.metrics.export.wavefront.enabled=false")
+@SpringBootTest
+class WavefrontAngularTracingDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
